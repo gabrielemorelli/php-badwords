f6aec2643bbfb84e8e838f98dd7c66c89b4bbe23
MyUsernme <gabriele.morelli@live.it>
2022-03-09 23:25:49 +0100

    esercizio

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+$stringaprincale = "nuovo esercizio con php";
+
+echo '<h1> Stringa principale </h1>';
+echo $stringaprincale;
+
+echo '<h2> Stringa principale </h2>';
+
+$stringamodificata = strlen($stringaprincale);
+echo $stringamodificata
+
+
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
